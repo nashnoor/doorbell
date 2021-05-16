382377c793eb7823635a82ed3c2cc430d4417e77
assynergy <77919034+assynergy@users.noreply.github.com>
2021-05-16 14:17:58 +0800

    Update app.py

--- webapp/app.py
+++ webapp/app.py
@@ -11,7 +11,7 @@ def ring():
    file = open(r'/home/pi/webapp/play.py', 'r').read()
    f=exec(file)
    return redirect(url_for('index')), f
-   
-
+  
 if __name__ == '__main__':
-   serve(app, host='0.0.0.0', port=5001)
+   #app.run(debug = True) // Uncomment if you want to use in Development Environment 
+   serve(app, host='0.0.0.0', port=5001) #For Production Environment 
