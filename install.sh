2cfc85456fa1d6380f079de01a16736d9347258e
assynergy <77919034+assynergy@users.noreply.github.com>
2021-12-19 22:11:59 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,3 +2,8 @@
 apt install python3 python3-pip 
 pip install flask waitress
 git clone https://github.com/assynergy/flask_app.git
+cd flask_app
+cp webapp /usr/bin/webapp
+cp doorbell.service /lib/systemd/system
+systemctl enable doorbell
+systemctl start doorbell
