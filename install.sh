a13b627f44f8ce6c752b1fa59322a6617fe14800
nasrulnoor <91359325+nasrulnoor@users.noreply.github.com>
2022-05-12 20:23:51 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 apt install python3 python3-pip git -y
 pip install flask waitress
-git clone https://github.com/assynergy/flask_app.git
-cd flask_app
-cp webapp /usr/bin/webapp
+git clone https://github.com/nasrulnoor/doorbell_flask.git
+cd doorbell_flask
+cp -r webapp /usr/bin/webapp
 cp doorbell.service /lib/systemd/system
 systemctl enable doorbell
 systemctl start doorbell
