bb02c7703fc0ea0c6343ba427a63169879a7dbc3
nasrulnoor <91359325+nasrulnoor@users.noreply.github.com>
2022-05-12 19:29:30 +0800

    Update app.py

--- webapp/app.py
+++ webapp/app.py
@@ -1,5 +1,10 @@
 from flask import Flask , render_template,redirect,url_for
 from waitress import serve
+import os 
+
+# x = os.getcwd() + "/" + "play.py"
+# print(x)
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -8,10 +13,10 @@ def index():
 
 @app.route('/ring')
 def ring():
+   file = str(os.system("aplay bell.wav"))
    file = open(r'/home/pi/doorbell_python_flask/webapp/play.py', 'r').read()
    f=exec(file)
    return redirect(url_for('index')), f
   
 if __name__ == '__main__':
-   #app.run(debug = True) // Uncomment if you want to use in Development Environment 
    serve(app, host='0.0.0.0', port=5001) #For Production Environment 
