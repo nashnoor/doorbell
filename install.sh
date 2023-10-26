3cbd50d5f93bb01c3e9954ec254a0b9c708ad60c
nashnoor <110758148+nashnoor@users.noreply.github.com>
2023-10-27 03:49:39 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 apt install python3 python3-pip git -y
 pip3 install flask waitress telegram-send
-git clone https://github.com/nasrulnoor/doorbell_flask.git
+git clone https://github.com/nashnoor/doorbell.git
 cd doorbell_flask
 #pip3 install -r requirements.txt
 cp -r webapp /usr/bin/webapp
