962e26d614c1cdb9091b08d4896177caec5891b0
nashnoor <110758148+nashnoor@users.noreply.github.com>
2023-10-27 03:54:03 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 apt install python3 python3-pip git -y
 pip3 install flask waitress telegram-send
 git clone https://github.com/nashnoor/doorbell.git
-cd doorbell_flask
+cd doorbell
 #pip3 install -r requirements.txt
 cp -r webapp /usr/bin/webapp
 cp doorbell.service /lib/systemd/system
